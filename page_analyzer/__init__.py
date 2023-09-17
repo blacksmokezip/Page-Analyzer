d8f30f3e803ce3a533cae561b3b70111e93db2f8
blacksmokezip <hanzomainer1488@gmail.com>
2023-09-17 21:19:34 +1000

    First commit

---
+++ page_analyzer/__init__.py
@@ -0,0 +1,3 @@
+from page_analyzer import app
+
+__all__ = ['app']
---
+++ settings.py
@@ -0,0 +1,5 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+SECRET_KEY = os.environ.get("SECRET_KEY")
+DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD")
