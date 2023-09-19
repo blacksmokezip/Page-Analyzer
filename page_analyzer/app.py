927b06076f463ac2f50a837f23a6a797cfc6092b
blacksmokezip <hanzomainer1488@gmail.com>
2023-09-19 22:50:24 +1000

    Fix makefile

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,8 +1,20 @@
 from flask import Flask
 
-app = Flask(__name__)
 
+def app(environ, start_response):
+    app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Welcome to Flask'
+    @app.route('/')
+    def hello_world():
+        return 'Welcome to Flask'
+
+    # data = 'Hello. World!'
+    #
+    # start_response("200 OK", [
+    #     ("Content-Type", "text/plain"),
+    #     ("Content-Length", str(len(data)))
+    # ])
+    return app.run()
+
+if __name__ == '__main__':
+    app()
