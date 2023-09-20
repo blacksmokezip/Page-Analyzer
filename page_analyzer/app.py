d93ab80e948e81d5c635c201e22642d1b2e8136e
blacksmokezip <hanzomainer1488@gmail.com>
2023-09-20 22:21:23 +1000

    Add main page

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Welcome to Flask'
+def main_page():
+    return render_template(
+        'index.html'
+    )
 
 
 if __name__ == "__main__":
